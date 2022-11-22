4da9970b5fbfbca6f4802867f3251d2f3cd21ab2
Albaer <alisi3396@gmail.com>
2022-11-22 16:35:34 +0100

    Start game response

---
+++ backend/src/main/java/com/codersdungeon/minesweeper/dtos/ResponseStartDTO.java
@@ -0,0 +1,16 @@
+package com.codersdungeon.minesweeper.dtos;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+public class ResponseStartDTO {
+    private LocalDateTime startDate;
+}
